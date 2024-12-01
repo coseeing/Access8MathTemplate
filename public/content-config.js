386e9a8daec908e3379e3e44c273ad5dcaef95fa
shinyu0430 <0430shinyu@gmail.com>
2024-12-01 22:59:24 +0800

    feat: process markdown image syntax

--- public/content-config.js
+++ public/content-config.js
@@ -1,1 +1,11 @@
-window.contentConfig = {"title":"Access8Math","latexDelimiter":"bracket","documentFormat":"block","documentDisplay":"markdown","sourceText":"## Question 1\n\n1. \\({{\\left( -3 \\right)}^{3}}\\)之值為何？\n(A) \\(-27\\)\n(B) \\(-9\\)\n(C) \\(9\\)\n(D) \\(27\\)\n2. aewf",documentColor: "dark"}
+window.contentConfig = {
+    "title":"Access8Math",
+    "latexDelimiter":"bracket",
+    "documentFormat":"block",
+    "documentDisplay":"markdown",
+    "sourceText":"## Question 1\n\n1. \\({{\\left( -3 \\right)}^{3}}\\)之值為何？\n(A) \\(-27\\)\n(B) \\(-9\\)\n(C) \\(9\\)\n(D) \\(27\\)\n2. ![picture of counting number 0 to 9 and math symbols](image_0)",
+    "documentColor": "dark",
+    "imageFileName": {
+        "image_0": "image_0.png"
+    }
+}
