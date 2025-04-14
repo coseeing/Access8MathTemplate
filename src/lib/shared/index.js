2f339ce3cfff957d5eb22f8f9d2c12e642e10c51
Wendell Liu <cuk.bas@gmail.com>
2025-04-14 18:59:03 +0800

    chore: demo code

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -1,9 +1,55 @@
-const markedProcessorFactory = require('./content-processor/markdown-process');
-const textProcessorFactory = require('./content-processor/text-process');
 const latexDelimiterConvertor = require('./content-processor/latext-delimiter-convertor');
 
+const createMarkdownParser = ({
+  options = {
+    latexDelimiter: null,
+    asciimathDelimiter: null,
+    htmlMathDisplay: null,
+    imageFiles: null,
+  },
+  components,
+}) => {
+  const parseMarkdown = () => {
+    // use options and components
+    console.log('Options:', options);
+    console.log('Components:', components);
+  };
+
+  return parseMarkdown;
+};
+
+// deprecate textProcessorFactory
 module.exports = {
-  markedProcessorFactory,
-  textProcessorFactory,
+  // rename to createMarkdownParser
+  createMarkdownParser,
   latexDelimiterConvertor,
 };
+
+// demo for createMarkdownParser
+const CustomImage = ({ src, alt }) => {
+  return `<img src="${src}" alt="${alt}" />`;
+};
+
+const CustomAlertSection = ({ title, content }) => {
+  return `<div class="alert-section">
+    <h2>${title}</h2>
+    <p>${content}</p>
+  </div>`;
+};
+
+const markdownContent = `# Hello World`;
+
+const options = {
+  latexDelimiter: 'bracket',
+  asciimathDelimiter: 'graveaccent',
+  htmlMathDisplay: 'block',
+  imageFiles: [],
+};
+
+const components = {
+  Image: CustomImage,
+  AlertSection: CustomAlertSection,
+};
+
+const parseMarkdown = createMarkdownParser({ options, components });
+const parsedContent = parseMarkdown(markdownContent);
