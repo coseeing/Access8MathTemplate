e9f71640fe38b09a68a8d09daa23e517456b58df
Wendell Liu <cuk.bas@gmail.com>
2025-09-05 00:45:15 +0800

    feat: bump to v1

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,39 +1,55 @@
-import React, { useMemo } from 'react';
-import { markedProcessorFactory } from '@coseeing/see-mark';
+import React, { useMemo, useCallback } from 'react';
+import { createMarkdownToReactParser } from '@coseeing/see-mark';
 
-import linkHandler, { useBindModalLinkEffect } from '@/lib/link';
+import { useBindModalLinkEffect } from '@/lib/link';
 import { useConfigContext } from '@/lib/context/config';
+
+import Alert from './Alert';
+import InternalLink from './InternalLink';
 
 function Content() {
   const {
-    data: { latexDelimiter, documentFormat, sourceText },
+    data: { latexDelimiter, documentFormat, sourceText, images },
   } = useConfigContext();
 
   useBindModalLinkEffect();
 
-  const markedFunc = useMemo(() => {
-    return markedProcessorFactory({
-      latexDelimiter: latexDelimiter,
-      asciimathDelimiter: 'graveaccent',
-      htmlMathDisplay: documentFormat,
-    });
-  }, [latexDelimiter, documentFormat]);
+  const seeMarkReactParse = useSeeMarkParse({
+    latexDelimiter,
+    documentFormat,
+    imageFiles: images,
+  });
 
-  const markdownHTML = useMemo(() => {
-    return linkHandler(markedFunc(sourceText));
-  }, [sourceText, markedFunc]);
+  // TODO: fulfill what linkHandler does
+  const content = useMemo(() => {
+    return seeMarkReactParse(sourceText);
+  }, [sourceText, seeMarkReactParse]);
 
   return (
     <div className="markdown">
       <div data-remove-styles>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: markdownHTML,
-          }}
-        />
+        <div>{content}</div>
       </div>
     </div>
   );
 }
 
 export default Content;
+
+const useSeeMarkParse = ({ latexDelimiter, documentFormat, imageFiles }) => {
+  const seeMarkReactParse = useCallback(
+    (markdown) => {
+      return createMarkdownToReactParser({
+        options: {
+          latexDelimiter,
+          documentFormat,
+          imageFiles,
+        },
+        components: { alert: Alert, internalLink: InternalLink },
+      })(markdown);
+    },
+    [imageFiles, latexDelimiter, documentFormat],
+  );
+
+  return seeMarkReactParse;
+};
