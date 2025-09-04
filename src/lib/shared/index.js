5642641019fce15f90268f1edbb10cf7c0a998b2
Wendell Liu <cuk.bas@gmail.com>
2025-09-05 01:12:05 +0800

    Integrate with See Mark V1 (#18)
    * feat: bump to v1
    
    * feat: update README

--- src/lib/shared/index.js
+++
@@ -1,9 +0,0 @@
-const markedProcessorFactory = require('./content-processor/markdown-process');
-const textProcessorFactory = require('./content-processor/text-process');
-const latexDelimiterConvertor = require('./content-processor/latext-delimiter-convertor');
-
-module.exports = {
-  markedProcessorFactory,
-  textProcessorFactory,
-  latexDelimiterConvertor,
-};
